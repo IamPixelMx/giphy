1d64c95d6e16b6ea2903c91a74e0ab9adb04e96d
Itzel Enciso <enciso.iq@gmail.com>
2020-07-14 18:32:27 -0500

    Save before format

--- src/App.js
+++ src/App.js
@@ -4,17 +4,17 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className='App'>
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
         <p>
-          Se adaptó  <code>src/App.js</code> para Giphy Engine.
+          Se adaptó <code>src/App.js</code> para Giphy Engine.
         </p>
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+          className='App-link'
+          href='https://reactjs.org'
+          target='_blank'
+          rel='noopener noreferrer'
         >
           Seguir
         </a>
